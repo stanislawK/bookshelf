d4de1ee5d0ef1557b88b182ee7afdea54cb707cf
stanislawK <stanislaw.krupienko@gmail.com>
2019-05-18 13:47:02 +0200

    enable to add more then one category to book form

--- blueprints/book.py
+++ blueprints/book.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, flash, jsonify, redirect, render_template, request
+from flask import Blueprint, flash, jsonify, render_template
 
 from bookshelf.forms.book_form import BookForm
 from bookshelf.models.author import AuthorModel
@@ -17,7 +17,7 @@ def add_book_form():
         title = book_form.title.data
         description = book_form.description.data
         authors = book_form.authors.data
-        category = book_form.categories.data
+        categories = book_form.categories.data
         new_book = BookModel(title=title, description=description)
         new_book.save_to_db()
 
@@ -28,11 +28,12 @@ def add_book_form():
                 author_db.save_to_db()
             new_book.add_author(author_db)
 
-        category_db = CategoryModel.find_by_name(category)
-        if not category_db:
-            category_db = CategoryModel(name=category)
-            category_db.save_to_db()
-        new_book.add_category(category_db)
+        for category in categories:
+            category_db = CategoryModel.find_by_name(category)
+            if not category_db:
+                category_db = CategoryModel(name=category)
+                category_db.save_to_db()
+            new_book.add_category(category_db)
 
         flash('New book successfully added', 'success')
 
@@ -43,5 +44,13 @@ def add_book_form():
 def add_author(id):
     book_form = BookForm()
     new_rows = [book_form.authors.append_entry() for _ in range(int(id))]
+    new_row = str(new_rows[-1])
+    return jsonify(new_row=new_row)
+
+
+@book_blueprint.route('/_add-category/<id>', methods=['GET'])
+def add_category(id):
+    book_form = BookForm()
+    new_rows = [book_form.categories.append_entry() for _ in range(int(id))]
     new_row = str(new_rows[-1])
     return jsonify(new_row=new_row)
