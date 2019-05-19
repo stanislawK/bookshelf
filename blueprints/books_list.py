8e70a50ca972ef00125b8a259d5008741c1ffa61
stanislawK <stanislaw.krupienko@gmail.com>
2019-05-19 20:19:12 +0200

    improved filtering

--- blueprints/books_list.py
+++ blueprints/books_list.py
@@ -18,15 +18,23 @@ def books():
 
     author = request.form.get('author_id')
     category = request.form.get('category_id')
-
+    clear = request.form.get('clear')
+    select = ''
     # Filter books by author
     if request.method == 'POST' and author:
         books = BookModel.find_by_author(author)
+        select = 'a{}'.format(author)
 
     if request.method == 'POST' and category:
         books = BookModel.find_by_category(category)
+        select = 'c{}'.format(category)
+
+    if request.method == 'POST' and clear:
+        books = BookModel.query.all()
+        select = ''
 
     return render_template('books.html',
                            books=books,
                            authors=authors,
-                           categories=categories)
+                           categories=categories,
+                           select=select)
